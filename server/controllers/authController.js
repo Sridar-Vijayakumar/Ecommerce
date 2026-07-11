01c5ccec11db2b1a53b3680d7c25ca2c1a49281b
Sridar Vijayakumar <sridarvijayakumar@gmail.com>
2026-07-11 14:38:44 +0530

    Create protect middleware and update controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -64,3 +64,27 @@ module.exports = {
   registerUser,
   loginUser,
 };
+
+const getUserProfile = async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } else {
+    res.status(404).json({
+      message: "User not found",
+    });
+  }
+};
+
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getUserProfile,
+};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const {
   registerUser,
   loginUser,
+  getUserProfile,
 } = require("../controllers/authController");
+
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", protect, getUserProfile);
 
 module.exports = router;

74a627538b5ab920897d55ad17c7953fd317b33b
Sridar Vijayakumar <sridarvijayakumar@gmail.com>
2026-07-30 13:55:16 +0530

    DEPLOYMENT 1

--- server/server.js
+++ server/server.js
@@ -49,7 +49,9 @@ app.get("/", (req, res) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+if (require.main === module) app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+module.exports = app;
 
